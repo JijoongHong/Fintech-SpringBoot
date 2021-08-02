2513835012e9d898737d3a37681e72ecd98c6077
JijoongHong <dsh1u234@gmail.com>
2021-08-02 23:55:35 +0900

    api

--- src/main/java/fintech/spring/repository/MemberRepository.java
+++ src/main/java/fintech/spring/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package fintech.spring.repository;
 import fintech.spring.entity.Member;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 //@RepositoryRestResource 자동생성 repository 인터페이스 하나로
+@Repository
 public interface MemberRepository extends PagingAndSortingRepository<Member,Long> {
+    //<대상, 킷값>
 }
